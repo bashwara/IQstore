96fe65e63f6fea3f554b267ed7b2ff340d48a631
Bashwara Undupitiya <bashwara@gmail.com>
2021-08-26 17:58:58 +0530

    Added search, sort & pagination

--- Core/Specifications/ProductsWithCategoriesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithCategoriesAndBrandsSpecification.cs
@@ -4,10 +4,33 @@ namespace Core.Specifications
 {
     public class ProductsWithCategoriesAndBrandsSpecification : BaseSpecification<Product>
     {
-        public ProductsWithCategoriesAndBrandsSpecification()
+        public ProductsWithCategoriesAndBrandsSpecification(ProductSpecParams productParams) 
+            : base(x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains(productParams.Search)) &&
+                (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) && 
+                (!productParams.CategoryId.HasValue || x.ProductCategoryId == productParams.CategoryId)
+            )
         {
             AddInclude(x => x.ProductCategory);
             AddInclude(x => x.ProductBrand);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex-1), productParams.PageSize);
+
+            if (!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch (productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithCategoriesAndBrandsSpecification(int id) : base(x => x.Id == id)
